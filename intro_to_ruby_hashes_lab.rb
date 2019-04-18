55958c9fcbbf4ee5235a53b2b80bfeb088a32506
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-18 18:28:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,17 +9,9 @@ end
 
 def monopoly
 	monopoly = {
-	  :railroads => {
-	    :pieces => 4,
-	    :rent_in_dollars{
-	      
-	    },
-	    
-	    :names => {
-	      
-	    }
-	    }
-	  }
+	  :railroads => {}
+	  :rent_in_dollars{}
+	  :names =>{}
 end
 
 def monopoly_with_second_tier
