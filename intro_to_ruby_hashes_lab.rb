a36f668fc8b643a6c72271e2a5eaca06f71064d1
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-18 18:22:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,9 @@ def monopoly
 	    :pieces => 4
 	    :rent_in_dollars =>{
 	      :one_piece_owned => 25,
-	      :two_pieces_owned => 50
-	      :three_pieces_owned => 100
-	      :four_pieces_owned => 200
+	      :two_pieces_owned => 50,
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200,
 	    }
 	  }
 	}
